963c563f75985684dc656fbc363d8b4f65a61de3
Raki Rahman <mdrrahman@microsoft.com>
2022-03-12 13:06:26 -0500

    Workaround for HTTPS issue on Ubuntu with Dotnet

--- PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -15,7 +15,19 @@ namespace PlatformService.SyncDataServices.Http
 
         public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
         {
-            _httpClient = httpClient;
+            // Dotnet issue on Ubuntu: 
+            // --> Could not send synchronously to Command Service: The SSL connection could not be established, see inner exception. | System.Security.Authentication.AuthenticationException: The remote certificate is invalid because of errors in the certificate chain: PartialChain
+            //    at System.Net.Security.SslStream.SendAuthResetSignal(ProtocolToken message, ExceptionDispatchInfo exception)
+            //    at System.Net.Security.SslStream.ForceAuthenticationAsync[TIOAdapter](TIOAdapter adapter, Boolean receiveFirst, Byte[] reAuthenticationData, Boolean isApm)
+            //    at System.Net.Http.ConnectHelper.EstablishSslConnectionAsyncCore(Boolean async, Stream stream, SslClientAuthenticationOptions sslOptions, CancellationToken cancellationToken)
+            
+            // Fix: https://stackoverflow.com/a/56460052/8954538
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            // Ignore SSL errors to return true
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            HttpClient client = new HttpClient(clientHandler);
+
+            _httpClient = client; // Override the default httpClient
             _configuration = configuration;
         }
 
